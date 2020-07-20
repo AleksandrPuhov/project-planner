3e9151f744431a1220e06b99e3d5fd6bfe00de30
AleksandrPuhov <aleksandr_puhov@list.ru>
2020-07-20 23:51:56 +0300

    drag and drop added

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,7 @@ class DOMHelper {
             newDestinationSelector
         );
         destinationElement.append(element);
+        element.scrollIntoView({ behavior: 'smooth' });
     }
 }
 
@@ -39,9 +40,10 @@ class Component {
 }
 
 class Tooltip extends Component {
-    constructor(closeNotifierFunction) {
-        super();
+    constructor(closeNotifierFunction, text, hostElementId) {
+        super(hostElementId);
         this.closeNotifier = closeNotifierFunction;
+        this.text = text;
         this.create();
     }
 
@@ -53,7 +55,24 @@ class Tooltip extends Component {
     create() {
         const tooltipElement = document.createElement('div');
         tooltipElement.className = 'card';
-        tooltipElement.textContent = 'My Text';
+
+        const tooltipTemplate = document.getElementById('tooltip');
+        const tooltipBody = document.importNode(tooltipTemplate.content, true);
+        tooltipBody.querySelector('p').textContent = this.text;
+        tooltipElement.append(tooltipBody);
+
+        const hostElPosLeft = this.hostElement.offsetLeft;
+        const hostElPosTop = this.hostElement.offsetTop;
+        const hostElHeight = this.hostElement.clientHeight;
+        const parentElScrolling = this.hostElement.parentElement.scrollTop;
+
+        const x = hostElPosLeft + 20;
+        const y = hostElPosTop + hostElHeight - parentElScrolling - 10;
+
+        tooltipElement.style.position = 'absolute';
+        tooltipElement.style.left = x + 'px';
+        tooltipElement.style.top = y + 'px';
+
         tooltipElement.addEventListener('click', this.closeTooltip.bind(this));
         this.element = tooltipElement;
         document.body.append(tooltipElement);
@@ -68,16 +87,32 @@ class ProjectItem {
         this.updateProjectListsHandler = updateProjectListsFunction;
         this.connectMoreInfoButton();
         this.connectSwitchButton(type);
+        this.connectDrag();
     }
 
     showMoreInfoHandler() {
         if (!this.hasActiveTooltip) {
-            const tooltip = new Tooltip(() => {
-                this.hasActiveTooltip = false;
-            });
+            const projectElement = document.getElementById(this.id);
+            const tooltipText = projectElement.dataset.extraInfo;
+            const tooltip = new Tooltip(
+                () => {
+                    this.hasActiveTooltip = false;
+                },
+                tooltipText,
+                this.id
+            );
             tooltip.attach();
             this.hasActiveTooltip = true;
         }
+    }
+
+    connectDrag() {
+        document
+            .getElementById(this.id)
+            .addEventListener('dragstart', (event) => {
+                event.dataTransfer.setData('text/plain', this.id);
+                event.dataTransfer.effectAllowed = 'move';
+            });
     }
 
     connectMoreInfoButton() {
@@ -85,7 +120,10 @@ class ProjectItem {
         const moreInfoBtn = projectItemElement.querySelector(
             'button:first-of-type'
         );
-        moreInfoBtn.addEventListener('click', this.showMoreInfoHandler);
+        moreInfoBtn.addEventListener(
+            'click',
+            this.showMoreInfoHandler.bind(this)
+        );
     }
 
     connectSwitchButton(type) {
@@ -106,6 +144,7 @@ class ProjectItem {
 
 class ProjectList {
     projects = [];
+
     constructor(type) {
         this.type = type;
         const prjItems = document.querySelectorAll(`#${type}-projects li`);
@@ -118,6 +157,42 @@ class ProjectList {
                 )
             );
         }
+        this.connectDroppable();
+    }
+
+    connectDroppable() {
+        const list = document.querySelector(`#${this.type}-projects ul`);
+
+        list.addEventListener('dragenter', (event) => {
+            if (event.dataTransfer.types[0] === 'text/plain') {
+                list.parentElement.classList.add('droppable');
+                event.preventDefault();
+            }
+        });
+
+        list.addEventListener('dragover', (event) => {
+            if (event.dataTransfer.types[0] === 'text/plain') {
+                event.preventDefault();
+            }
+        });
+        list.addEventListener('dragleave', (event) => {
+            if (
+                event.relatedTarget.closest(`#${this.type}-projects ul`) !==
+                list
+            ) {
+                list.parentElement.classList.remove('droppable');
+            }
+        });
+        list.addEventListener('drop', (event) => {
+            const prjId = event.dataTransfer.getData('text/plain');
+            if (!this.projects.find((p) => p.id === prjId)) {
+                document
+                    .getElementById(prjId)
+                    .querySelector('button:last-of-type')
+                    .click();
+            }
+            list.parentElement.classList.remove('droppable');
+        });
     }
 
     setSwitchHandlerFunction(switchHandlerFunction) {
